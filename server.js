44caf7e1f92f2411630b4f8f6fa15cd828cfdd44
Abdulla Durrani <Abdullahkd@gmail.com>
2024-11-12 13:44:24 -0500

    committing server page

--- server.js
+++ server.js
@@ -2,19 +2,72 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello There!');
+  res.render('homepage.ejs');
 });
 
-// rendering 'home page'
-app.get('/Homepage', (req, res) => {
-    res.render('homepage.ejs');
-  });
+// // rendering 'home page'
+// app.get('/Homepage', (req, res) => {
+//     res.render('homepage.ejs');
+//   });
 
 // rendering 'company history'  
 app.get('/companyHistory', (req, res) => {
     res.render('company-history.ejs');
 });
 
+// rendering 'past work'
+app.get('/pastWork', (req, res) => {
+    res.render('past-work.ejs');
+});
+
+// rendering 'Staff'
+app.get('/staff', (req, res) => {
+    res.render('staff.ejs', {
+        staff: STAFF.members
+    });
+});
+
+
+// rendering Contact Us'
+app.get('/contact', (req, res) => {
+    res.render('contact-us.ejs');
+});
+
+// rendering 'External Links'
+app.get('/externalLinks', (req, res) => {
+    res.render('external-links.ejs');
+});
+
+
+const STAFF = {
+   members : [
+    {
+        name: 'Bugs Bunny',
+        position: 'CEO',
+        salary: '1.5 million',
+        tenure: '86 Years of Service'
+    },
+    {
+        name: 'Elmer Fudd',
+        position: 'CFO',
+        salary: '1 million',
+        tenure: '87 Years of Service'
+    },
+    {
+        name: 'Daffy Duck',
+        position: 'CTO',
+        salary: '800K',
+        tenure: '87 Years of Service'
+    },
+    {
+        name: 'Tazmanian Devil (Taz)',
+        position: 'Head of Security',
+        salary: '80K',
+        tenure: '70 Years of Service'
+    }
+
+   ]
+}
 
 
 
